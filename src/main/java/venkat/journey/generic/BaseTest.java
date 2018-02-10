4ecc6a8e741c9a2e4364fd3031068075adbfe82a
venkat9014 <31129932+venkat9014@users.noreply.github.com>
2018-02-10 11:32:46 +0530

    gg

--- src/main/java/venkat/journey/generic/BaseTest.java
+++ src/main/java/venkat/journey/generic/BaseTest.java
@@ -10,13 +10,14 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
-//@Listeners(CustomListener.class)
+@Listeners(CustomListener.class)
 
 public class BaseTest implements InterfaceConstants {
 	public WebDriver driver;
